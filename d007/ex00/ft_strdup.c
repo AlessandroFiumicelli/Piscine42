265232c5beec195aa2a9f4eb09f5494905612c5b
Alessandro FIUMICELLI <alfiumic@e2r13p3.42.fr>
2018-09-08 18:38:58 +0200

    add ex07

---
+++ d007/ex00/ft_strdup.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alfiumic <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/07 16:21:50 by alfiumic          #+#    #+#             */
+/*   Updated: 2018/09/08 12:35:53 by alfiumic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		ft_strlen(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		size_src;
+	char	*dup;
+
+	size_src = ft_strlen(src);
+	if (!(dup = (char*)malloc(sizeof(char) * (size_src + 1))))
+		return ("NULL");
+	return (ft_strcpy(dup, src));
+}
