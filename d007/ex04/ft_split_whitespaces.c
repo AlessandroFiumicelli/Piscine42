e7d66e40b3520945557fbb17082680711f643362
Alessandro FIUMICELLI <alfiumic@e2r13p3.42.fr>
2018-09-11 15:28:19 +0200

    add

--- d007/ex04/ft_split_whitespaces.c
+++ d007/ex04/ft_split_whitespaces.c
@@ -6,7 +6,7 @@
 /*   By: alfiumic <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/10 14:54:24 by alfiumic          #+#    #+#             */
-/*   Updated: 2018/09/10 17:52:56 by alfiumic         ###   ########.fr       */
+/*   Updated: 2018/09/11 15:24:13 by alfiumic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ int		ft_count_word(char *str)
 		cnt++;
 	}
 	return (num);
-}/*
+}
 char	*ft_strncpy(char *dest, char *src, int size)
 {
 	int		i;
@@ -46,22 +46,62 @@ char	*ft_strncpy(char *dest, char *src, int size)
 	return (dest);
 }
 
+int		ft_custom_strlen(char *str, int word)
+{
+	int		i;
+	int		j;
+	int		wrdlen;
+
+	i = 0;
+	j = 0;
+
+	while (j < word)
+	{ 
+		if (i == 0 && str[i] > 32)
+			i++;
+		if (str[i] > 32 && (str[i - 1] == 9
+					|| str[i - 1] == 10 || str[i - 1] == 11
+					|| str[i - 1] == 32))
+		{
+			j++;
+		}
+		i++;
+	}
+	while (str[i] != 32 || str[i] > 11 || str[i] < 9)
+	{
+		i++;
+		wrdlen++;
+	}
+	return (worlen);`
+}
+
 char	**ft__split_whitespaces(char *str)
 {
 	int		cnt;
 	int		n_word;
 	char	**matrix;
+	int		lenword;
 
-	t
-
+	n_word = ft_count_word(str);
 	if (n_word == 0)
 		return ("NULL");
-	matrix = (char **)malloc(sizeof(char *) * n_word + 1);
-}*/
+	if (!(matrix = (char**)malloc(sizeof(**matrix) * n_word + 1)))
+		return (0);
+	cnt = 1;
+	lenword = 1;
+	while (cnt < n_word)
+	{
+		if (!(matrix = (char*)malloc(sizeof(char) * ft_custom_strlen(str, lenword) + 1)))
+			return (0);
+		cnt++;
+		lenword++;
+	}
+	return (matrix);
+}
 
 int		main(int argc, char **argv)
 {
-	if (argc == 1)
+	if (argc != 2)
 		return (0);
 	printf("%d",ft_count_word(argv[1]));
 	return (0);
